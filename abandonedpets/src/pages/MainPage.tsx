26b8248a0ab30737cec49fc5ce4ce22d7955cc99
JunYoung02 <whwnsdud011@naver.com>
2024-03-27 20:56:14 +0900

    chore: 프론트 초기 세팅

---
+++ abandonedpets/src/pages/MainPage.tsx
@@ -0,0 +1,5 @@
+function MainPage() {
+    return <div>메인페이지입니다</div>;
+}
+
+export default MainPage;
---
+++ abandonedpets/src/pages/TestPage.tsx
@@ -0,0 +1,5 @@
+function TestPage() {
+    return <div>테스트페이지입니다.</div>;
+}
+
+export default TestPage;
