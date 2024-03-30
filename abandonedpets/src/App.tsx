33384e43319ff0532b59b8ec324439a7000e0854
JunYoung02 <whwnsdud011@naver.com>
2024-03-31 01:44:59 +0900

    docs: 테스트 파일삭제

--- abandonedpets/src/App.tsx
+++ abandonedpets/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import TestPage from "./pages/TestPage";
 
 function App() {
   return (
@@ -8,7 +7,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/test" element={<TestPage />} />
         </Routes>
       </BrowserRouter>
     </>
