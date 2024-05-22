ee1b28ac5222bf916ce9579f6ea0aa421a45ea90
JunYoung02 <whwnsdud011@naver.com>
2024-05-23 01:45:02 +0900

    design: 유기동물 상세페이지 디자인 수정

--- abandonedpets/src/components/petDetail/ImgList.tsx
+++ abandonedpets/src/components/petDetail/ImgList.tsx
@@ -1,28 +1,21 @@
 import styled from 'styled-components';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 import DogList1 from '../../assets/sampleImg/DogList1.png';
 import DogList2 from '../../assets/sampleImg/DogList2.png';
+import Cat from '../../assets/sampleImg/Cat.png';
 
 const Wrapper = styled.div`
-  width: 60%;
+  width: 50%;
   margin: 3% auto;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr); /* 5개의 동일한 크기의 열 생성 */
-  grid-auto-flow: row; /* 요소가 너비를 초과하면 다음 행으로 넘어가도록 설정 */
-  gap: 20px;
-`;
-
-const Img = styled.img`
-  width: 100%;
-  height: auto;
-  object-fit: scale-down;
 `;
 
 const DogList = {
   Dogs: [
     { img: DogList1 },
-    { img: DogList2 },
+    { img: Cat },
     { img: DogList1 },
-    { img: DogList2 },
+    { img: Cat },
     { img: DogList1 },
     { img: DogList2 },
     { img: DogList1 },
@@ -33,9 +26,13 @@ const DogList = {
 function ImgList() {
   return (
     <Wrapper>
-      {DogList.Dogs.map((dog, index) => (
-        <Img src={dog.img} key={index} />
-      ))}
+      <ImageList variant="masonry" cols={3} gap={8}>
+        {DogList.Dogs.map((dog, index) => (
+          <ImageListItem key={index}>
+            <img src={dog.img} />
+          </ImageListItem>
+        ))}
+      </ImageList>
     </Wrapper>
   );
 }
