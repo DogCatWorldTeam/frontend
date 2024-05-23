2ac427b1eff888e54efc5046672d9bfbb22f8645
LeeJiWon <dlwldnjs1009@gmail.com>
2024-05-23 18:18:54 +0900

    design: 로그인 페이지 디자인 수정

--- abandonedpets/src/components/login/LoginForm.tsx
+++ abandonedpets/src/components/login/LoginForm.tsx
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 const LoginContainer = styled.div`
   display: flex;
@@ -12,42 +15,45 @@ const Explan = styled.div`
 `;
 
 const FormContainer = styled.form`
+  width: 18rem;
   display: flex;
   flex-direction: column;
   margin-top: 3%;
+  gap: 20px;
 `;
 
-const Input = styled.input`
-  width: 18rem;
-  height: 3.5rem;
-  font-size: 1rem;
-  color: #000;
-  border: none;
-  border-bottom: 1px solid #8f8f8f;
-  outline: none;
-  padding: 2%;
-  margin-top: 2%;
-`;
+// const Input = styled.input`
+//   width: 18rem;
+//   height: 3.5rem;
+//   font-size: 1rem;
+//   color: #000;
+//   border: none;
+//   border-bottom: 1px solid #8f8f8f;
+//   outline: none;
+//   padding: 2%;
+//   margin-top: 2%;
+// `;
 
-const Button = styled.button`
-  width: 18rem;
-  height: 2.5rem;
-  background-color: #ffdfbf;
-  font-size: 1rem;
-  border: none;
-  border-radius: 70px;
-  margin-top: 10%;
-  cursor: pointer;
-
-  &:active {
-    transform: scale(0.95);
-  }
-`;
+// const Button = styled.button`
+//   width: 18rem;
+//   height: 2.5rem;
+//   background-color: #ffdfbf;
+//   font-size: 1rem;
+//   border: none;
+//   border-radius: 70px;
+//   margin-top: 10%;
+//   cursor: pointer;
+//
+//   &:active {
+//     transform: scale(0.95);
+//   }
+// `;
 
 const SignUpContainer = styled.div`
   width: 18rem;
   display: flex;
   justify-content: space-between;
+  align-items: center;
   margin-top: 1.5%;
 `;
 
@@ -56,9 +62,11 @@ const SignUpExplan = styled.p`
   color: #939393;
 `;
 
-const SignUpText = styled.p`
+const SignUpText = styled(Link)`
   font-size: 0.875rem;
   cursor: pointer;
+  text-decoration: none;
+  color: inherit;
 `;
 
 function LoginForm() {
@@ -66,13 +74,15 @@ function LoginForm() {
     <LoginContainer>
       <Explan>로그인</Explan>
       <FormContainer>
-        <Input type="text" placeholder="이메일" />
-        <Input type="password" placeholder="비밀번호" />
-        <Button>로그인</Button>
+        <TextField variant="standard" label="이메일" />
+        <TextField variant="standard" label="비밀번호" type="password" />
+        <Button variant="contained" color="warning" size="large">
+          로그인
+        </Button>
       </FormContainer>
       <SignUpContainer>
         <SignUpExplan>아직 회원이 아니신가요?</SignUpExplan>
-        <SignUpText>회원가입</SignUpText>
+        <SignUpText to="/signup">회원가입</SignUpText>
       </SignUpContainer>
     </LoginContainer>
   );
