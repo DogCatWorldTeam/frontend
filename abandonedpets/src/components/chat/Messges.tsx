935c2b92ccb8096d9aadb59792b039a1d7dd9e3d
JunYoung02 <whwnsdud011@naver.com>
2024-05-26 23:56:53 +0900

    design: 채팅 내용 디자인 수정

--- abandonedpets/src/components/chat/Messges.tsx
+++ abandonedpets/src/components/chat/Messges.tsx
@@ -3,11 +3,44 @@ import styled from 'styled-components';
 const MessagesContainer = styled.div`
   height: 530px;
   overflow-y: auto;
-  margin: 10px 0;
+  margin: 20px 0 10px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const Message = styled.div`
+  max-width: 60%; /* 최대 너비를 설정하여 메시지가 너무 길어지지 않도록 함 */
+  padding: 10px;
+  font-size: 0.875rem;
+  border-radius: 8px;
+  word-wrap: break-word; /* 단어가 박스를 넘지 않도록 함 */
+`;
+
+const SendMessage = styled(Message)`
+  align-self: flex-end;
+  background-color: #4784fb;
+  color: #fff;
+  border-radius: 8px 8px 0 8px;
+`;
+
+const ReceiveMessage = styled(Message)`
+  align-self: flex-start;
+  background-color: #e2e2e2;
+  color: #000;
+  border-radius: 8px 8px 8px 0;
 `;
 
 function Messages() {
-  return <MessagesContainer>채팅 메세지</MessagesContainer>;
+  return (
+    <MessagesContainer>
+      <SendMessage>안녕하세요 ddddddddddddddddddddddddddddddddd</SendMessage>
+      <ReceiveMessage>
+        네 안녕하세요! ddddddddddddddddddddddddddddddddd
+      </ReceiveMessage>
+      <ReceiveMessage>무슨일이세요?</ReceiveMessage>
+    </MessagesContainer>
+  );
 }
 
 export default Messages;
