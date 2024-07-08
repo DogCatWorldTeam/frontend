1c2eb48e4a3a7eece64db9275384fc20bb31b0a9
JunYoung <whwnsdud011@naver.com>
2024-07-08 14:02:40 +0900

    design: 글 작성 페이지 반응형 적용

--- abandonedpets/src/components/petWrite/Form.tsx
+++ abandonedpets/src/components/petWrite/Form.tsx
@@ -26,6 +26,7 @@ const FromHeader = styled.div`
 
 const Title = styled.div`
   width: 100%;
+  min-width: 300px;
   font-size: 2em;
   font-weight: 900;
   display: flex;
@@ -41,7 +42,7 @@ const LeftContainer = styled.div`
 `;
 
 const BtnContainer = styled.div`
-  width: 17rem;
+  width: 17em;
   border: 1px solid #ffbe57;
   margin-bottom: 0.25rem;
   border-radius: 6px;
@@ -68,8 +69,9 @@ const StyledButton = styled.button<{ choice: boolean }>`
 `;
 
 const ImgFile = styled.label`
-  width: 45rem;
-  height: 30rem;
+  width: 90%;
+  min-width: 300px;
+  height: 30em;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -103,12 +105,13 @@ const InputFile = styled.input`
 
 const InfoContainer = styled.div`
   width: 50%;
+  min-width: 300px;
   display: flex;
   justify-content: space-evenly;
 `;
 
 const InfoDetail = styled.div`
-  width: 15rem;
+  width: 15em;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
@@ -144,10 +147,11 @@ const InputBtnContainer = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
+  gap: 5px;
 `;
 
 const InputGenderBtn = styled.button<{ gender: boolean }>`
-  width: 4rem;
+  width: 4em;
   border: none;
   border-radius: 5px;
   color: #000;
@@ -162,7 +166,7 @@ const InputGenderBtn = styled.button<{ gender: boolean }>`
 `;
 
 const InputNeuterBtn = styled.button<{ neuter: boolean | string }>`
-  width: 4rem;
+  width: 4em;
   border: none;
   border-radius: 5px;
   color: #000;
@@ -174,6 +178,11 @@ const InputNeuterBtn = styled.button<{ neuter: boolean | string }>`
     cursor: pointer;
     background-color: #ffbe57;
   }
+`;
+
+const MapContainer = styled.div`
+  width: 100%;
+  min-width: 300px;
 `;
 
 const TextArea = styled.textarea`
@@ -200,17 +209,20 @@ const TextArea = styled.textarea`
 `;
 
 const InputImgList = styled.div`
-  width: 100%;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-auto-flow: row; /* 요소가 너비를 초과하면 다음 행으로 넘어가도록 설정 */
   gap: 20px;
   place-items: center;
+
+  @media screen and (max-width: 1023px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
 
 const ImgPreviewList = styled.label`
-  width: 15rem;
-  height: 15rem;
+  width: 15em;
+  height: 15em;
   border: 1px solid #ffc184;
   border-radius: 10px;
   display: flex;
@@ -439,8 +451,8 @@ function Form() {
         .post('/api/v1/pet_board/create', data, {
           headers: { 'Content-Type': 'multipart/form-data' },
         })
-        .then((res) => {
-          console.log(res);
+        .then(() => {
+          // console.log(res);
           alert('글 작성 완료');
           navigate('/dog');
         });
@@ -524,6 +536,12 @@ function Form() {
         <InfoContainer>
           <InfoDetail>
             <PetDetailText>동물 종류</PetDetailText>
+            <PetDetailText>이름</PetDetailText>
+            <PetDetailText>성별</PetDetailText>
+            <PetDetailText>나이</PetDetailText>
+            <PetDetailText>몸무게</PetDetailText>
+            <PetDetailText>품종</PetDetailText>
+            <PetDetailText>중성화 여부</PetDetailText>
           </InfoDetail>
 
           <InfoDetail>
@@ -543,20 +561,6 @@ function Form() {
                 고양이
               </InputGenderBtn>
             </InputBtnContainer>
-          </InfoDetail>
-        </InfoContainer>
-
-        <InfoContainer>
-          <InfoDetail>
-            <PetDetailText>이름</PetDetailText>
-            <PetDetailText>성별</PetDetailText>
-            <PetDetailText>나이</PetDetailText>
-            <PetDetailText>몸무게</PetDetailText>
-            <PetDetailText>품종</PetDetailText>
-            <PetDetailText>중성화 여부</PetDetailText>
-          </InfoDetail>
-
-          <InfoDetail>
             <InputInfo
               name="name"
               placeholder="이름"
@@ -634,18 +638,18 @@ function Form() {
         </InfoContainer>
       </FromHeader>
 
-      <div>
+      <MapContainer>
         <div>입양자와 만나기 좋은 장소를 골라주세요</div>
         <Map
           center={position}
-          style={{ width: '800px', height: '600px' }}
+          style={{ width: '100%', height: '37em' }}
           level={7}
           onClick={handleClick}
         >
           <MapMarker position={position} />
         </Map>
         {address && <div>{(address as any).address_name}</div>}
-      </div>
+      </MapContainer>
 
       <TextArea
         name="specialMark"
@@ -683,184 +687,3 @@ function Form() {
 }
 
 export default Form;
-
-// function Form() {
-//   const [isSelected, setIsSelected] = useState<string | null>(null); // 입양 상태 버튼
-//   const [imgFile, setImgFile] = useState<string | null>(null); // 썸네일 미리보기
-//   const [imgList, setImgList] = useState<(string | undefined)[]>([]); // 이미지 리스트
-//   const [isGender, setIsGender] = useState<string | null>(null); // 성별 체크
-//   const [isNeuter, setIsNeuter] = useState<string | null>(null); // 중성화 체크
-
-//   const imgRef = useRef<HTMLInputElement>(null);
-
-//   const btnClickHandler = (choice: string) => {
-//     setIsSelected(choice);
-//   };
-
-//   const genderBtnHendler = (gender: string) => {
-//     setIsGender(gender);
-//   };
-
-//   const neuterBtnHandler = (neuter: string) => {
-//     setIsNeuter(neuter);
-//   };
-
-//   // 썸네일 미리보기
-//   const handleFileChange = () => {
-//     if (imgRef.current && imgRef.current.files) {
-//       const file: File | null = imgRef.current.files[0]; // 파일 가져오기
-//       if (file) {
-//         const reader = new FileReader();
-//         reader.readAsDataURL(file); // 파일 읽기
-//         reader.onloadend = () => {
-//           const result: string | null = reader.result as string; // 결과
-//           setImgFile(result); // 이미지 파일 설정
-//         };
-//       }
-//     }
-//   };
-
-//   // 이미지 리스트 미리보기
-//   const imgListHandler = (
-//     event: React.ChangeEvent<HTMLInputElement>,
-//     index: number,
-//   ) => {
-//     const file = event.target.files?.[0];
-//     if (file) {
-//       const reader = new FileReader();
-//       reader.onloadend = () => {
-//         setImgList((prevImgList) => {
-//           const newList = [...prevImgList];
-//           newList[index] = reader.result as string;
-//           return newList;
-//         });
-//       };
-//       reader.readAsDataURL(file);
-//     }
-//   };
-
-//   return (
-//     <FormWrapper>
-//       <FromHeader>
-//         <Title>
-//           <TextField
-//             fullWidth
-//             label="제목"
-//             variant="outlined"
-//             color="warning"
-//           />
-//         </Title>
-
-//         <LeftContainer>
-//           <BtnContainer>
-//             <StyledButton
-//               type="button"
-//               choice={isSelected === 'yes'} // 선택된 버튼인지 확인하여 부울 값으로 전달
-//               onClick={() => btnClickHandler('yes')}
-//             >
-//               입양 대기
-//             </StyledButton>
-//             <StyledButton
-//               type="button"
-//               choice={isSelected === 'no'} // 선택된 버튼인지 확인
-//               onClick={() => btnClickHandler('no')}
-//             >
-//               입양 완료
-//             </StyledButton>
-//           </BtnContainer>
-//           <ImgFile htmlFor="petImg">
-//             {imgFile ? (
-//               <ImgPreview src={imgFile} alt="Preview" />
-//             ) : (
-//               <ImgFileExplan>여기를 클릭해서 사진을 올려주세요.</ImgFileExplan>
-//             )}
-//           </ImgFile>
-
-//           <InputFile
-//             type="file"
-//             id="petImg"
-//             onChange={handleFileChange}
-//             ref={imgRef}
-//           />
-//         </LeftContainer>
-
-//         <InfoContainer>
-//           <InfoDetail>
-//             <PetDetailText>이름</PetDetailText>
-//             <PetDetailText>성별</PetDetailText>
-//             <PetDetailText>나이</PetDetailText>
-//             <PetDetailText>몸무게</PetDetailText>
-//             <PetDetailText>품종</PetDetailText>
-//             <PetDetailText>중성화 여부</PetDetailText>
-//             <PetDetailText>주소</PetDetailText>
-//           </InfoDetail>
-
-//           <InfoDetail>
-//             <InputInfo placeholder="이름" />
-//             <InputBtnContainer>
-//               <InputGenderBtn
-//                 type="button"
-//                 gender={isGender === 'male'}
-//                 onClick={() => genderBtnHendler('male')}
-//               >
-//                 남
-//               </InputGenderBtn>
-//               <InputGenderBtn
-//                 type="button"
-//                 gender={isGender === 'female'}
-//                 onClick={() => genderBtnHendler('female')}
-//               >
-//                 여
-//               </InputGenderBtn>
-//             </InputBtnContainer>
-//             <InputInfo type="text" placeholder="나이" />
-//             <InputInfo type="text" placeholder="몸무게" />
-//             <InputInfo type="text" placeholder="품종" />
-//             <InputBtnContainer>
-//               <InputNeuterBtn
-//                 type="button"
-//                 neuter={isNeuter === 'yes'}
-//                 onClick={() => neuterBtnHandler('yes')}
-//               >
-//                 네
-//               </InputNeuterBtn>
-//               <InputNeuterBtn
-//                 type="button"
-//                 neuter={isNeuter === 'no'}
-//                 onClick={() => neuterBtnHandler('no')}
-//               >
-//                 아니요
-//               </InputNeuterBtn>
-//             </InputBtnContainer>
-//             <InputInfo type="text" placeholder="주소" />
-//           </InfoDetail>
-//         </InfoContainer>
-//       </FromHeader>
-
-//       <TextArea placeholder="추가 설명을 작성해주세요. (예, 성격 또는 특이사항)" />
-
-//       <InputImgList>
-//         {[...Array(6)].map((_, index) => (
-//           <ImgPreviewList key={`listImg${index}`} htmlFor={`listImg${index}`}>
-//             {imgList[index] ? (
-//               <ImgPreview src={imgList[index]} alt="Preview" />
-//             ) : (
-//               <ImgFileExplan>여기를 클릭해서 사진을 올려주세요.</ImgFileExplan>
-//             )}
-//             <InputFile
-//               type="file"
-//               id={`listImg${index}`}
-//               onChange={(e) => imgListHandler(e, index)}
-//             />
-//           </ImgPreviewList>
-//         ))}
-//       </InputImgList>
-
-//       <Button variant="contained" color="warning">
-//         <SubmitBtn>등록하기</SubmitBtn>
-//       </Button>
-//     </FormWrapper>
-//   );
-// }
-
-// export default Form;
