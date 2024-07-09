c77400b197679529f7442e660c022a77be8c5861
JunYoung <whwnsdud011@naver.com>
2024-07-09 15:14:14 +0900

    design: 장례식장 페이지 반응형 구현

--- abandonedpets/src/components/funeral/FuneralList.tsx
+++ abandonedpets/src/components/funeral/FuneralList.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { useTransition, animated } from '@react-spring/web';
-import Stack from '@mui/material/Stack';
+import { useTransition } from '@react-spring/web';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { CardActionArea, Grid, ImageList, ImageListItem } from '@mui/material';
+import { CardActionArea } from '@mui/material';
 import axios from 'axios';
+import CardMedia from '@mui/material/CardMedia';
 
 // 스타일드 컴포넌트 정의
 const PageWrapper = styled.div`
@@ -18,23 +18,51 @@ const PageWrapper = styled.div`
   background-color: #f5f5f5;
 `;
 
-const AreaWrapper = styled.div`
-  width: 80%;
-  margin: 2rem 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 40px;
-  padding: 2% 0;
+// const AreaWrapper = styled.div`
+//   max-width: 75%;
+//   width: 100%;
+//   margin: 2em 0;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   gap: 40px;
+//   padding: 2% 0;
+// `;
+
+// const StackWrapper = styled(Stack)`
+//   margin-top: 2rem;
+// `;
+
+const BtnWrapper = styled.div`
+  display: -webkit-box;
+  margin: 2em;
+
+  & > *:not(:last-child) {
+    margin-right: 16px; /* 원하는 간격을 설정 */
+  }
 `;
 
-const StackWrapper = styled(Stack)`
-  margin-top: 2rem;
-`;
+// const CustomImageList = styled(ImageList)`
+//   :last-child {
+//     margin-bottom: 16px;
+//   }
+// `;
 
-const CustomImageList = styled(ImageList)`
-  :last-child {
-    margin-bottom: 16px;
+const ItemContainer = styled.div`
+  display: grid;
+  max-width: 80%;
+  width: 100%;
+  grid-template-columns: repeat(3, 1fr); // 3개의 열
+  place-items: center;
+  gap: 15px;
+  margin: 3% auto;
+
+  @media screen and (max-width: 1023px) {
+    grid-template-columns: repeat(2, 1fr); // 2개의 열
+  }
+
+  @media screen and (max-width: 767px) {
+    grid-template-columns: repeat(1, 1fr); // 1개의 열
   }
 `;
 
@@ -92,7 +120,7 @@ function FuneralList() {
 
   return (
     <PageWrapper>
-      <StackWrapper spacing={2} direction="row" justifyContent="center">
+      <BtnWrapper>
         {[
           '세종',
           '부산',
@@ -110,82 +138,86 @@ function FuneralList() {
             key={region}
             variant={selectedRegion === region ? 'contained' : 'outlined'}
             onClick={() => handleButtonClick(region)}
+            sx={{ marginBottom: '0.75em' }}
           >
             {region}
           </Button>
         ))}
-      </StackWrapper>
-      <AreaWrapper>
-        <Grid container spacing={2}>
-          <CustomImageList variant="masonry" cols={4}>
-            {transitions((style, item) => (
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                md={4}
-                lg={3}
-                key={item ? item.id : 'empty'}
-              >
-                <animated.div style={style}>
-                  {item && (
-                    <Card sx={{ minWidth: 280 }}>
-                      <CardActionArea
-                        component="a"
-                        href={item.homepage}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {/* <CardMedia
-                        component="img"
-                        image={item.image}
-                        alt="Funeral Image"
-                        /> */}
-                        <ImageListItem key={item.image}>
-                          <img
-                            src={item.image}
-                            alt={item.funeralName}
-                            loading="lazy"
-                          />
-                        </ImageListItem>
-                        <CardContent>
-                          <Typography
-                            gutterBottom
-                            variant="h5"
-                            component="div"
-                            sx={{
-                              whiteSpace: 'nowrap',
-                              overflow: 'hidden',
-                              textOverflow: 'ellipsis',
-                            }}
-                          >
-                            {item.funeralName}
-                          </Typography>
-                          <Typography
-                            variant="body2"
-                            color="text.secondary"
-                            sx={{
-                              display: '-webkit-box',
-                              WebkitBoxOrient: 'vertical',
-                              overflow: 'hidden',
-                              textOverflow: 'ellipsis',
-                              WebkitLineClamp: 2, // Adjust the number of lines you want to show
-                            }}
-                          >
-                            {item.phoneNum}
-                            <br />
-                            {item.address}
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                  )}
-                </animated.div>
-              </Grid>
-            ))}
-          </CustomImageList>
-        </Grid>
-      </AreaWrapper>
+      </BtnWrapper>
+      {/* <AreaWrapper> */}
+      <ItemContainer>
+        {/* <CustomImageList variant="masonry" cols={4}> */}
+        {transitions((_, item) => (
+          // <Grid
+          //   item
+          //   xs={2}
+          //   sm={4}
+          //   md={4}
+          //   // lg={3}
+          //   key={item ? item.id : 'empty'}
+          // >
+          <>
+            {/* <animated.div style={style}> */}
+            {item && (
+              <Card sx={{ width: '100%' }}>
+                <CardActionArea
+                  href={item.homepage}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <CardMedia
+                    height="200"
+                    component="img"
+                    image={item.image}
+                    alt={item.funeralName}
+                    sx={{ objectFit: 'contain' }}
+                  />
+                  {/* <ImageListItem key={item.image}>
+                    <img
+                      src={item.image}
+                      alt={item.funeralName}
+                      loading="lazy"
+                    />
+                  </ImageListItem> */}
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                      sx={{
+                        whiteSpace: 'nowrap',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                      }}
+                    >
+                      {item.funeralName}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      sx={{
+                        display: '-webkit-box',
+                        WebkitBoxOrient: 'vertical',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        WebkitLineClamp: 2, // Adjust the number of lines you want to show
+                      }}
+                    >
+                      {item.phoneNum}
+                      <br />
+                      {item.address}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            )}
+            {/* </animated.div> */}
+          </>
+          // </Grid>
+        ))}
+        {/* </CustomImageList> */}
+      </ItemContainer>
+      {/* </AreaWrapper> */}
     </PageWrapper>
   );
 }
